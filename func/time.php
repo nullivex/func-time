db70b861c1291263f5be9ccdf48c7f641a0dc021
Nullivex <contact@nullivex.com>
2013-03-12 22:36:07 -0700

    refactored func.php into packages, renamed functions, updated error handling, upgraded boot process

---
+++ func/time.php
@@ -0,0 +1,31 @@
+<?php
+
+//Date and Time related utility functions
+function age($tm,$end=false,$rcs = 0) {
+	if(!$end) $end = time();
+	$cur_tm = $end; $dif = $cur_tm-$tm;
+	$pds = array('second','minute','hour','day','week','month','year','decade');
+	$lngh = array(1,60,3600,86400,604800,2630880,31570560,315705600);
+	for($v = sizeof($lngh)-1; ($v >= 0)&&(($no = $dif/$lngh[$v])<=1); $v--); if($v < 0) $v = 0; $_tm = $cur_tm-($dif%$lngh[$v]);
+	$no = floor($no); if($no <> 1) $pds[$v] .='s'; $x=sprintf("%d %s ",$no,$pds[$v]);
+	if(($rcs == 1)&&($v >= 1)&&(($cur_tm-$_tm) > 0)) $x .= time_ago($_tm);
+	return $x;
+}
+
+function future_age($tm,$end=false,$rcs = 0) {
+	if(!$end) $end = time();
+	$cur_tm = $end; $dif = ($cur_tm-$tm)*-1;
+	$pds = array('second','minute','hour','day','week','month','year','decade');
+	$lngh = array(1,60,3600,86400,604800,2630880,31570560,315705600);
+	for($v = sizeof($lngh)-1; ($v >= 0)&&(($no = $dif/$lngh[$v])<=1); $v--); if($v < 0) $v = 0; $_tm = $cur_tm-($dif%$lngh[$v]);
+	$no = floor($no); if($no <> 1) $pds[$v] .='s'; $x=sprintf("%d %s ",$no,$pds[$v]);
+	if(($rcs == 1)&&($v >= 1)&&(($cur_tm-$_tm) > 0)) $x .= time_ago($_tm);
+	return $x;
+}
+
+//Formatters
+function hours($tm,$inc=1){
+	$hours = round($tm / round((3600 / $inc)));
+	if($hours < 1) $hours = 1;
+	return $hours.' hours';
+}

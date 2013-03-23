fd999bebbac2720a395ee63d7a8df098c945bdbc
nullivex <contact@nullivex.com>
2013-03-22 23:42:45 -0700

    removed buggy parts

--- func/time.php
+++ func/time.php
@@ -1,25 +1,23 @@
 <?php
 
 //Date and Time related utility functions
-function age($tm,$end=false,$rcs = 0) {
+function age($tm,$end=false) {
 	if(!$end) $end = time();
 	$cur_tm = $end; $dif = $cur_tm-$tm;
 	$pds = array('second','minute','hour','day','week','month','year','decade');
 	$lngh = array(1,60,3600,86400,604800,2630880,31570560,315705600);
 	for($v = sizeof($lngh)-1; ($v >= 0)&&(($no = $dif/$lngh[$v])<=1); $v--); if($v < 0) $v = 0; $_tm = $cur_tm-($dif%$lngh[$v]);
 	$no = floor($no); if($no <> 1) $pds[$v] .='s'; $x=sprintf("%d %s ",$no,$pds[$v]);
-	if(($rcs == 1)&&($v >= 1)&&(($cur_tm-$_tm) > 0)) $x .= time_ago($_tm);
 	return $x;
 }
 
-function future_age($tm,$end=false,$rcs = 0) {
+function future_age($tm,$end=false) {
 	if(!$end) $end = time();
 	$cur_tm = $end; $dif = ($cur_tm-$tm)*-1;
 	$pds = array('second','minute','hour','day','week','month','year','decade');
 	$lngh = array(1,60,3600,86400,604800,2630880,31570560,315705600);
 	for($v = sizeof($lngh)-1; ($v >= 0)&&(($no = $dif/$lngh[$v])<=1); $v--); if($v < 0) $v = 0; $_tm = $cur_tm-($dif%$lngh[$v]);
 	$no = floor($no); if($no <> 1) $pds[$v] .='s'; $x=sprintf("%d %s ",$no,$pds[$v]);
-	if(($rcs == 1)&&($v >= 1)&&(($cur_tm-$_tm) > 0)) $x .= time_ago($_tm);
 	return $x;
 }
 
